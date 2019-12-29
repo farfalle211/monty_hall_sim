99603ecebdbe8bd9e9df9241c9197c8e95f942c9
farfalle211 <farfalle211@gmail.com>
2019-12-28 22:29:38 -0600

    final solution in trials.rb

--- trials.rb
+++ trials.rb
@@ -1,15 +1,11 @@
 class MontyHall
-  attr_accessor :doors, :initial_pick, :initial_pick_index, :final_choice
-
-  # @@swap_win_counter = 0
-  # @@no_swap_win_counter = 0
-  # @@total_no_swap_runs = 0
-  # @@total_swap_runs = 0
+  attr_accessor :doors, :initial_pick, :initial_pick_index, :revealed_door
 
   def initialize
     @doors = ["goat", "goat", "car"].shuffle
     @initial_pick_index = rand(2)
     @initial_pick = doors[initial_pick_index]
+    @revealed_door = ""
   end
 
   def reveal_door
@@ -17,7 +13,7 @@ class MontyHall
     #Cannot be the player's original choice or door with the car behind it
     door_indexes = [0, 1, 2]
     door_indexes -= [initial_pick_index, doors.index("car")]
-    @revealed_door = door_indexes.shuffle.first
+    self.revealed_door = door_indexes.shuffle.first
   end
 
   def no_swap_win?
@@ -26,12 +22,15 @@ class MontyHall
   end
 
   def swap_win?
-    swapped_index = ([0, 1, 2] - [initial_pick_index, @revealed_door]).first
+    #Find the index of the swap option
+    #Swap option is not the initial pick or the revealed door
+    swapped_index = ([0, 1, 2] - [initial_pick_index, revealed_door]).first
     final_choice = doors[swapped_index]
     won?(final_choice)
   end
 
   def won?(final_choice)
+    #True if the final choice is a car
     final_choice == "car"
   end
 end
@@ -44,7 +43,7 @@ class Tester
     @no_swap_wins = 0
     @trials = 10000
 
-    @trials.times do
+    trials.times do
       monty_hall = MontyHall.new
       monty_hall.reveal_door
       @swap_wins += 1 if monty_hall.swap_win?
@@ -54,26 +53,28 @@ class Tester
   end
 
   def calulate_win_stats
+    #Every trial can only have one winning scenario
+    #Divide the number of wins for swap/no_swap by the number of trials
+    #Multiply by 100 for a percentage
     @swap_win_percent = (swap_wins.to_f / trials) * 100
     @no_swap_win_percent = (no_swap_wins.to_f / trials) * 100
   end
 
-  def print_results
+  def swap_results
     puts "Swap win percent is #{@swap_win_percent.round(2)}%"
+  end
+
+  def no_swap_results
     puts "No Swap win percent is #{@no_swap_win_percent.round(2)}%"
   end
 
   def send_results
-    File.open("results.jpg", "w") {|f| f.write "#{print_results}"}
+    File.open("results.txt", "w") do |file| 
+      file.write "#{swap_results}\n"
+      file.write "#{no_swap_results}"
+    end
   end
 end
 
 tester = Tester.new
 tester.send_results
-
-# MontyHall.run
-# MontyHall.stats
-
-# input = 3
-# string = "p 'hello' * #{input}"
-# eval(string)
