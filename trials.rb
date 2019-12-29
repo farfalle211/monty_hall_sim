a453c274de2fc2c9ebe1ccede6b9df60d9a49319
farfalle211 <farfalle211@gmail.com>
2019-12-28 22:52:17 -0600

    Cleaned up comments

--- trials.rb
+++ trials.rb
@@ -1,6 +1,12 @@
+### Solution to the Monty Hall Problem
+### Author: Michael Dessailly, email: dessaillym@gmail.com
+### Date: 12/27/2019
+
 class MontyHall
   attr_accessor :doors, :initial_pick, :initial_pick_index, :revealed_door
 
+  #Shuffle the arrangement behind the doors
+  #Randomize the initial selection of the player
   def initialize
     @doors = ["goat", "goat", "car"].shuffle
     @initial_pick_index = rand(2)
@@ -8,29 +14,30 @@ class MontyHall
     @revealed_door = ""
   end
 
+  #Find the index of the host's revealed door
+  #This cannot be the player's original choice or the door with the car behind it
   def reveal_door
-    #Find the index of the revealed door
-    #Cannot be the player's original choice or door with the car behind it
     door_indexes = [0, 1, 2]
     door_indexes -= [initial_pick_index, doors.index("car")]
     self.revealed_door = door_indexes.shuffle.first
   end
 
+  #True if the initial pick is the car
   def no_swap_win?
     final_choice = initial_pick
     won?(final_choice)
   end
 
+  #True if swapping results in the car
+  #Find the index of Swap option; this isn't the initial pick or the revealed door
   def swap_win?
-    #Find the index of the swap option
-    #Swap option is not the initial pick or the revealed door
     swapped_index = ([0, 1, 2] - [initial_pick_index, revealed_door]).first
     final_choice = doors[swapped_index]
     won?(final_choice)
   end
 
+  #True if the final choice is a car
   def won?(final_choice)
-    #True if the final choice is a car
     final_choice == "car"
   end
 end
@@ -38,6 +45,8 @@ end
 class Tester
   attr_accessor :swap_wins, :no_swap_wins, :trials
 
+  #Initialize a counter for swap wins and no_swap wins
+  #Run the MontyHall simulation @trials times and track swap/no_swap wins
   def initialize
     @swap_wins = 0
     @no_swap_wins = 0
@@ -52,22 +61,21 @@ class Tester
     calulate_win_stats
   end
 
+  #Obtain win percentages for swap/no_swap scenarios
   def calulate_win_stats
-    #Every trial can only have one winning scenario
-    #Divide the number of wins for swap/no_swap by the number of trials
-    #Multiply by 100 for a percentage
     @swap_win_percent = (swap_wins.to_f / trials) * 100
     @no_swap_win_percent = (no_swap_wins.to_f / trials) * 100
   end
 
   def swap_results
-    puts "Swap win percent is #{@swap_win_percent.round(2)}%"
+    "Swap win percent is #{@swap_win_percent.round(2)}%"
   end
 
   def no_swap_results
-    puts "No Swap win percent is #{@no_swap_win_percent.round(2)}%"
+    "No Swap win percent is #{@no_swap_win_percent.round(2)}%"
   end
 
+  #Write results to results.txt file
   def send_results
     File.open("results.txt", "w") do |file| 
       file.write "#{swap_results}\n"
