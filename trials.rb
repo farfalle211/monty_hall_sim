172849a044f5ae3cb1c81c085a376f2990be8bd2
farfalle211 <farfalle211@gmail.com>
2020-01-13 17:06:49 -0500

    removed attr_accessor methods and changed to instance variables

--- trials.rb
+++ trials.rb
@@ -3,14 +3,13 @@
 ### Date: 12/27/2019
 
 class MontyHall
-  attr_accessor :doors, :initial_pick, :initial_pick_index, :revealed_door
 
   #Shuffle the arrangement behind the doors
   #Randomize the initial selection of the player
   def initialize
-    @doors = ["goat", "goat", "car"].shuffle
-    @initial_pick_index = rand(2)
-    @initial_pick = doors[initial_pick_index]
+    @doors = [:goat, :goat, :car].shuffle
+    @initial_pick_index = rand(3)
+    @initial_pick = @doors[@initial_pick_index]
     @revealed_door = ""
   end
 
@@ -18,32 +17,31 @@ class MontyHall
   #This cannot be the player's original choice or the door with the car behind it
   def reveal_door
     door_indexes = [0, 1, 2]
-    door_indexes -= [initial_pick_index, doors.index("car")]
-    self.revealed_door = door_indexes.shuffle.first
+    door_indexes -= [@initial_pick_index, @doors.index(:car)]
+    @revealed_door = door_indexes.shuffle.first
   end
 
   #True if the initial pick is the car
   def no_swap_win?
-    final_choice = initial_pick
+    final_choice = @initial_pick
     won?(final_choice)
   end
 
   #True if swapping results in the car
   #Find the index of Swap option; this isn't the initial pick or the revealed door
   def swap_win?
-    swapped_index = ([0, 1, 2] - [initial_pick_index, revealed_door]).first
-    final_choice = doors[swapped_index]
+    swapped_index = ([0, 1, 2] - [@initial_pick_index, @revealed_door]).first
+    final_choice = @doors[swapped_index]
     won?(final_choice)
   end
 
   #True if the final choice is a car
   def won?(final_choice)
-    final_choice == "car"
+    final_choice == :car
   end
 end
 
 class Tester
-  attr_accessor :swap_wins, :no_swap_wins, :trials
 
   #Initialize a counter for swap wins and no_swap wins
   #Run the MontyHall simulation @trials times and track swap/no_swap wins
@@ -52,7 +50,7 @@ class Tester
     @no_swap_wins = 0
     @trials = 10000
 
-    trials.times do
+    @trials.times do
       monty_hall = MontyHall.new
       monty_hall.reveal_door
       @swap_wins += 1 if monty_hall.swap_win?
@@ -63,8 +61,8 @@ class Tester
 
   #Obtain win percentages for swap/no_swap scenarios
   def calulate_win_stats
-    @swap_win_percent = (swap_wins.to_f / trials) * 100
-    @no_swap_win_percent = (no_swap_wins.to_f / trials) * 100
+    @swap_win_percent = (@swap_wins.to_f / @trials) * 100
+    @no_swap_win_percent = (@no_swap_wins.to_f / @trials) * 100
   end
 
   def swap_results
